c93cadb701b08e4bfa39a836d599253c5a0041ac
wz <970007876@qq.com>
2022-06-07 08:19:20 +0800

    Initial commit

---
+++ src/main/java/com/mcs/McsMobilenurseServer01Application.java
@@ -0,0 +1,13 @@
+package com.mcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsMobilenurseServer01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McsMobilenurseServer01Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/mcs/bean/user.java
@@ -0,0 +1,8 @@
+package com.mcs.bean;
+
+public class user {
+
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
